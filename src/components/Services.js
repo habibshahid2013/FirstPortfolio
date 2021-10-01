09697055b104f168319d57380ad0d296554b341d
Hassan Shahid <habibshahid2013@gmail.com>
2021-09-30 23:30:33 -0500

    added exprience changes

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { faDesktop, faFileCode } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons"
+import { faDesktop, faFileCode,  fa-network-wired} from "@fortawesome/free-solid-svg-icons";
+
+
 
 
 const Services = () => {
@@ -10,34 +12,40 @@ const Services = () => {
            <div className="py-5">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-4 col-sm-6">
                         <div className="box">
        <div className="circle"><FontAwesomeIcon icon={faDesktop} size="2x"/></div>
-                            <h3>Web Design</h3>
-                            <p>I approach each project indiviually and always focus on the result</p>
+                            <h3>Software Engineering </h3>
+                            <p>I'm versed through various languages needed to do Full-Stack Engineering. After being trained at Prime Digital Academy. I'm certified to to be a Full-Stack Software Engineer. </p>
+                            <p><u> Software Engineer Skill-Set</u></p>
+                            <p>Node.js, PostgresSQL, HTML, CSS, JQuery, JavaScript, React, Redux, Saga, JSON, Git, Heroku, Bootstrap, Material UI, AJAX, REST, Express, Firebase,.Net, C#  </p>
+
 
                         </div>
                     </div>
                     {/*   */}
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-4 col-sm-6">
                         <div className="box">
-         <div className="circle"><FontAwesomeIcon icon={faFileCode} size="2x"/></div>
-                            <h3>Web Design</h3>
-                            <p>Your website will be popping hello yay yas yas</p>
+         <div className="circle"><FaNetworkWired  icon={FaNetworkWired} size="2x"/></div>
+                            <h3>Network Engineering</h3>
+                            <p>Training and working within the Networking Field. I am CCNA Qualified. Applying my knowledge of routing and switching into various Data Center environments.  </p>
+                            <p><u>Network Engineer Technologies</u></p>
+                            <ul className="techlist">
+                            <li>Routing and Switching</li>
+                            <li> Cisco ACI</li>
+                            <li>Firewall (ASA, Firepower, and Pal-Alto)</li>
+                            <li> Cisco Wireless</li>
+                            <li>SD-Wan</li>
+                            <li>Cisco Nexus (VDC, VPC, and FEX)</li>
+                            <li>WAN and LAN</li>
+                            <li>SNMP/Netflow</li> 
+                            <li>Infoblox</li> 
+                            </ul> 
 
                         </div>
                     </div>
                      {/*   */}
-                    <div className="col-md-3 col-sm-6">
-                        <div className="box">
-         <div className="circle"><FontAwesomeIcon icon={faFileCode} size="2x"/></div>
-                            <h3>Web Development</h3>
-                            <p>Dont forget we about to be getting monety out here chea!!!</p>
-
-                        </div>
-                    </div>
-                     {/*   */}
-                    <div className="col-md-3 col-sm-6">
+                    <div className="col-md-4 col-sm-6">
                         <div className="box">
          <div className="circle"><FontAwesomeIcon icon={faFileCode} size="2x"/></div>
                             <h3>Network Engineering</h3>
